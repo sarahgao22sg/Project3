58aab4b98b582f91f0b20b70a2169663cfca0aa8
jobongie <68445190+jobongie@users.noreply.github.com>
2021-01-19 22:00:40 -0600

    More main.py

--- main.py
+++ main.py
@@ -9,3 +9,27 @@ app.secret_key = SECRET_KEY
 login_manager = LoginManager(app)
 users = npipkins@yahoo.com
 password_login = password1!
+
+# Tableau server
+server = TSC.Server('http://10.0.55.1')
+folder_path = "C:/Users/Administrator/Desktop/Flask-WebServer/static/images"
+
+# Class User which extends UserMixin used to register users.5765
+# Probably converts to Tableau compatable class/object
+class User(UserMixin):
+    # Constructor
+    def __init__(self, user_id):
+        self.id = user_id
+        self.name = users[int(user_id)]
+
+    def get(self, name):
+        return users.index(name)
+
+        # Check if url to render is safe
+
+# Query address        
+def is_safe_url(target):
+    ref_url = urlparse(request.host_url)
+    test_url = urlparse(urljoin(request.host_url, target))
+    return test_url.scheme in ('http', 'https') and \
+        ref_url.netloc == test_url.netloc
