0b2ad1e15b52247be6efdab8648d94019c2d7150
jobongie <68445190+jobongie@users.noreply.github.com>
2021-01-22 18:09:16 -0600

    HTML and app.py changes

--- app.py
+++ app.py
@@ -5,112 +5,30 @@ import requests
 
 
 app = Flask(__name__)
-app.config['secret_key'] = b'randomstring'
-login_manager = LoginManager(app)
-users = ["npipkins@yahoo.com"]
-password_login = "password_1"
-
-# Tableau server
-# server = TSC.Server('http://10.0.55.1')
-folder_path = "C:/Users/Administrator/Desktop/Flask-WebServer/static/images"
-
-# Class User which extends UserMixin used to register users.5765
-# Probably converts to Tableau compatable class/object
-class User(UserMixin):
-    # Constructor
-    def __init__(self, user_id):
-        self.id = user_id
-        self.name = users[int(user_id)]
-
-    def get(self, name):
-        return users.index(name)
-        # Check if url to render is safe
-
-# Query address        
-def is_safe_url(target):
-    ref_url = urlparse(request.host_url)
-    test_url = urlparse(urljoin(request.host_url, target))
-    return test_url.scheme in ('http', 'https') and \
-        ref_url.netloc == test_url.netloc
-
-# Route for handling the login page logic
-# Checks if user exists and password matches
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if (request.method == 'POST'):
-        if (request.form['username'] not in users or request.form['password'] != password_login):
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            user = User(users.index(request.form['username']))
-            login_user(user)
-            next = request.args.get('next')
-
-            if not is_safe_url(next):
-                return abort(400)
-
-            resp = make_response(redirect('/homepage'))
-
-# Wrong assumption
-# Used to by Flask to check which is the current logged user.
-@login_manager.user_loader
-def load_user(user_id):
-    return User(user_id)
 
 # Route to redirect - when the onload event occurs - to the login page directly
 @app.route("/")
 def hello():
     # See line 40
-    return redirect(url_for('login'))
-
-@app.route("/api/data")
-def data():
-
-    return jsonify(table_df.to_dict(orient='records'))
-
-# Must establish server link, probably rename select-dashboard
-@app.route("/homepage")
-@login_required
-def homepage(): #index with all the dashboards
-
-    return render_template("select-dashboard.html")
+    return render_template("index.html")
 
 # Successive app routes, flaskin it
-@app.route("/Economic_Impact")    #page of a single dashboard
+@app.route("/Economic impact")    #page of a single dashboard
 @login_required
 def dashboard():
     # Verify data storage type/structure here
-    return render_template("Economic_Impact.html")
+    return render_template("Economic impact.html")
 
-@app.route("/Green_Frogs")    #page of a single dashboard
+@app.route("/Frog and Zebra Muscles Graph")    #page of a single dashboard
 @login_required
 def frogs():
     # Verify data storage type/structure here
-    return render_template("Economic_Impact.html")
+    return render_template("Frog and Zebra Muscles Graph.html")
 
-@app.route("/Invasion!")    #page of a single dashboard
+@app.route("/Density")    #page of a single dashboard
 @login_required
 def invasion():
-    return render_template("dashboard.html")
-
-# Ends Session for Tableau I suppose
-@app.route('/logout')
-def sign_out():
-    logout_user()
-    return redirect(url_for('login'))
-
-# Promise? Get-Response
-@app.route('/get_token')
-def get_token():
-    resp = make_response()
-
-    token = requests.post("http://10.0.55.1/trusted?username=" + request.cookies.get("username"))
-    if (token.status_code != 200 or token.text == '-1'): #200 = OK in HTTP requests
-        # Console.log error codes possibly- do check
-        return abort(400)
-
-    resp.set_cookie("auth_token", token.text)
-    return resp
+    return render_template("Density.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
